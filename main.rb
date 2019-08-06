4e2d8bafab9a943b09e9ef23b26dc55d4a83d953
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2019-08-06 16:03:40 +0100

    Show the percentage of repositories without an owner

--- main.rb
+++ main.rb
@@ -1,6 +1,29 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+# STEP 1: List repos with no owners
+def list_of_repos
+  directory = File.join(Dir.pwd, 'artifacts', 'STEP-1')
+  Dir.mkdir(directory) unless Dir.exist?(directory)
+
+  file_path = File.join(directory, 'repos.rbb')
+
+  if File.exist?(file_path)
+    content = File.read(file_path)
+
+    state = Marshal.load(content)
+  else
+    state = `cd ./workspace/github-tools/ruby && ruby -Isrc scripts/list.rb repos --all`
+    content = Marshal.dump(state)
+
+    File.write(file_path, content)
+  end
+
+  state
+end
+
+result_all = list_of_repos.lines(chomp: true)
+
 # STEP 2: List repos with no owners
 def list_of_repos_with_no_owners
   directory = File.join(Dir.pwd, 'artifacts', 'STEP-2')
@@ -22,11 +45,12 @@ def list_of_repos_with_no_owners
   state
 end
 
-result = list_of_repos_with_no_owners
-puts result
+result_no_owners = list_of_repos_with_no_owners.lines(chomp: true)
 
 # ...
 # STEP n: PROFIT!
+
+percentage = result_no_owners.size.to_f / result_all.size.to_f * 100
 
 require 'slack-ruby-client'
 Slack.configure do |config|
@@ -37,12 +61,25 @@ client = Slack::Web::Client.new
 client.auth_test
 
 channel_name = '#pe-offsites'
+
+require 'date'
+date = Date.today.to_s
+
 text = <<~HEREDOC
   These are the repos without a codeowner:
+  #{percentage.round(2)}% have no codeowner
 
   ```
-  #{result}
+  #{result_no_owners.join(',')}
   ```
 HEREDOC
 
 client.chat_postMessage(channel: channel_name, text: text)
+# client.files_upload(
+#   channels: channel_name,
+#   content: text,
+#   as_user: true,
+#   title: "SHAME! #{date}",
+#   filename: "shamelist-#{date}.txt",
+#   initial_comment: 'These are the repos without a codeowner'
+# )
